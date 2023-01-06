0be9de06118bbea05ce6faaf2a7677c29391adbf
andreiDmitriev2022 <107244116+andreiDmitriev2022@users.noreply.github.com>
2023-01-06 15:50:32 +0500

    добавлен калькулятор каллорий

--- js/script.js
+++ js/script.js
@@ -438,4 +438,79 @@ document.addEventListener('DOMContentLoaded', () => {
 		removeActive(dotArr);
 		addActive(dotArr, slideIndex);
 	});
+
+	//Calc
+
+	const result = document.querySelector(`.calculating__result span`);
+	let sex = `female`,
+		height, weight, age,
+		ratio = 1.375;
+
+	function calcTotal() {
+		if (!sex || !height || !weight || !age) {
+			result.textContent = `_____`;
+			return;
+		}
+		const female = Math.round((447.6 + (9.2 * weight) + (3.1 * height) - (4.3 * age)) * ratio);
+
+		const male = Math.round((88.36 + (13.4 * weight) + (4.8 * height) - (5.7 * age)) * ratio);
+
+		result.textContent = (sex === `female`) ? female : male;	
+	}
+
+	calcTotal();
+
+	function getStaticInformation(parentSelector, activeClass) {
+		const elements = document.querySelectorAll(`${parentSelector} div`);
+
+		elements.forEach((el) => {
+			el.addEventListener(`click`, (e) => {
+				if (e.target.getAttribute(`data-ratio`)) {
+					ratio = +e.target.getAttribute(`data-ratio`);
+				} else {
+					sex = e.target.getAttribute(`id`);
+				}
+
+				elements.forEach((el) => {
+					el.classList.remove(activeClass);
+				});
+
+				e.target.classList.add(activeClass);
+
+				calcTotal();
+			});
+		});
+	}
+
+	getStaticInformation(`#gender`, `calculating__choose-item_active`);
+	getStaticInformation(`.calculating__choose_big`,`calculating__choose-item_active`);
+
+	function getDynamicInformation(selector) {
+		const input = document.querySelector(selector);
+
+		input.addEventListener(`input`, (e) => {
+			const value = e.target.value;
+			console.log(value);
+
+			switch (e.target.getAttribute(`id`)) {
+				case `height`:
+					height = value;
+					break;
+
+				case `weight`:
+					weight = value;
+					break;
+
+				case `age`:
+					age = value;
+					break;
+			}
+
+			calcTotal();
+		});
+	}
+
+	getDynamicInformation(`#height`);
+	getDynamicInformation(`#weight`);
+	getDynamicInformation(`#age`);
 });
